55cb060a21ce93305d2efc3747a9b502d2a02346
stix2you <stix2you@gmail.com>
2024-04-22 15:09:50 -0500

    build out ShoppingLists component

--- components/ShoppingLists.js
+++ components/ShoppingLists.js
@@ -1,8 +1,28 @@
 import { StyleSheet, View, FlatList, Text } from 'react-native';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { collection, getDocs } from 'firebase/firestore';
 
 const ShoppingLists = ({ db }) => {
    const [lists, setLists] = useState([]);
+
+   // fetch all shopping lists from Firestore asynchronously, then call this within useEffect() 
+   // can't use async directly in useEffect()
+   const fetchShoppingLists = async () => {
+      const listsDocuments = await getDocs(collection(db, "shoppingLists"));
+      let newLists = [];
+      listsDocuments.forEach(docObject => {
+         newLists.push({ id: docObject.id, ...docObject.data() });
+      });
+      setLists(newLists);
+   }
+
+   useEffect(() => {
+      fetchShoppingLists();
+   }, [`${lists}`]);  // when 'lists' state changes, force a render cycle
+   // use ${lists} instead of {lists} to avoid memory reference issues (stringify the complex data-type variable)
+
+
+
 
    return (
       <View style={Styles.container}>
