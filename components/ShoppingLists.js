d04dd3ea5c1ec810c1845be58620aafe88de763f
stix2you <stix2you@gmail.com>
2024-04-22 17:24:31 -0500

    implement ADD function to lists in ShoppingLists.js

--- components/ShoppingLists.js
+++ components/ShoppingLists.js
@@ -1,9 +1,15 @@
-import { StyleSheet, View, FlatList, Text } from 'react-native';
+import { StyleSheet, View, FlatList, Text, TextInput, KeyboardAvoidingView, TouchableOpacity, Platform, Alert } from 'react-native';
 import { useState, useEffect } from 'react';
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, getDocs, addDoc } from 'firebase/firestore';
 
 const ShoppingLists = ({ db }) => {
+   // state for shopping lists
    const [lists, setLists] = useState([]);
+
+   // states for form inputs, which will become database fields
+   const [listName, setListName] = useState("");
+   const [item1, setItem1] = useState("");
+   const [item2, setItem2] = useState("");
 
    // fetch all shopping lists from Firestore asynchronously, then call this within useEffect() 
    // can't use async directly in useEffect()
@@ -21,25 +27,114 @@ const ShoppingLists = ({ db }) => {
    }, [`${lists}`]);  // when 'lists' state changes, force a render cycle
    // use ${lists} instead of {lists} to avoid memory reference issues (stringify the complex data-type variable)
 
+   // takes a new object structured as a shopping list (newList) and adds the data to Firestore
+   const addShoppingList = async (newList) => {
+      const newListRef = await addDoc(collection(db, "shoppinglists"), newList);
+      if (newListRef.id) {
+         setLists([newList, ...lists]);   // force a re-render with the new list added (change state of lists array)
+         Alert.alert(`The list "${listName}" has been added.`);
+      } else {
+         Alert.alert("Unable to add. Please try later");
+      }
+   }
+
    return (
       <View style={styles.container}>
          <FlatList
+            style={styles.listsContainer}
             data={lists}
             renderItem={({ item }) =>
-               // array function join() converts an array into a string
-               <Text>{item.name}: {item.items.join(", ")}</Text>}
+               <View style={styles.listItem}>
+                  <Text >{item.name}: {item.items.join(", ")}</Text>
+               </View>
+            }
          />
+         <View style={styles.listForm}>
+            <TextInput
+               style={styles.listName}
+               placeholder="List Name"
+               value={listName}
+               onChangeText={setListName}
+            />
+            <TextInput
+               style={styles.item}
+               placeholder="Item #1"
+               value={item1}
+               onChangeText={setItem1}
+            />
+            <TextInput
+               style={styles.item}
+               placeholder="Item #2"
+               value={item2}
+               onChangeText={setItem2}
+            />
+            <TouchableOpacity
+               style={styles.addButton}
+               onPress={() => {
+                  const newList = {  // this is where we are creating the new list object, assign to newList
+                     name: listName,
+                     items: [item1, item2]
+                  }  
+                  addShoppingList(newList);   // call the addShoppingList function with the newList object
+               }}
+            >
+               <Text style={styles.addButtonText}>Add</Text>
+            </TouchableOpacity>
+         </View>
+         {Platform.OS === "ios" ? <KeyboardAvoidingView behavior="padding" /> : null}
       </View>
-   );
+   )
 }
 
 const styles = StyleSheet.create({
    container: {
-      flex: 1,
-      // backgroundColor: '#fff',
-      // alignItems: 'center',
-      // justifyContent: 'center',
+      flex: 1
    },
+   listItem: {
+      height: 70,
+      justifyContent: "center",
+      paddingHorizontal: 30,
+      borderBottomWidth: 1,
+      borderBottomColor: "#AAA",
+      flex: 1,
+      flexGrow: 1
+   },
+   listForm: {
+      flexBasis: 275,
+      flex: 0,
+      margin: 15,
+      padding: 15,
+      backgroundColor: "#CCC"
+   },
+   listName: {
+      height: 50,
+      padding: 15,
+      fontWeight: "600",
+      marginRight: 50,
+      marginBottom: 15,
+      borderColor: "#555",
+      borderWidth: 2
+   },
+   item: {
+      height: 50,
+      padding: 15,
+      marginLeft: 50,
+      marginBottom: 15,
+      borderColor: "#555",
+      borderWidth: 2
+   },
+   addButton: {
+      justifyContent: "center",
+      alignItems: "center",
+      height: 50,
+      backgroundColor: "#000",
+      color: "#FFF"
+   },
+   addButtonText: {
+      color: "#FFF",
+      fontWeight: "600",
+      fontSize: 20
+   }
 });
 
 export default ShoppingLists;
